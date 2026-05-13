d1c7144b024ee671b00adc3e0b4da8798db36ae5
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-05-13 09:49:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -107,12 +107,13 @@ Send a file to get started.
 ✨ <i>Where files transform with magical precision!</i>"""
 
     HELP_TXT = """
-<b>•></b> 𝚂𝚎𝚗𝚍 /autorename 𝙵𝚘𝚛 𝚊𝚞𝚝𝚘 𝚁𝚎𝚗𝚊𝚖𝚎 𝚑𝚎𝚕𝚙 𝙰𝚗𝚍 𝚜𝚎𝚝𝚝𝚒𝚗𝚐 𝚊𝚞𝚝𝚘 𝚛𝚎𝚗𝚊𝚖𝚎.
+<b>•></b> 𝚂𝚎𝚗𝚍 /autorename 𝚏𝚘𝚛 𝚊𝚞𝚝𝚘-𝚛𝚎𝚗𝚊𝚖𝚎 𝚑𝚎𝚕𝚙 𝚊𝚗𝚍 𝚜𝚎𝚝𝚝𝚒𝚗𝚐𝚜.
 
 ✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
-<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ\nAɴᴅ 𝚂ᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].           
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ\nAɴᴅ 𝙸𝚝 𝚆𝚒𝚕𝚕 𝙱𝚎 𝙰𝚞𝚝𝚘𝚖𝚊𝚝𝚒𝚌𝚊𝚕𝚕𝚢 𝚁𝚎𝚗𝚊𝚖𝚎𝚍.           
 ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/DigitalBotz_Support>𝑺𝑼𝑷𝑷𝑶𝑹𝑻 𝑮𝑹𝑶𝑼𝑷</a>
 """
+
 
     
     
